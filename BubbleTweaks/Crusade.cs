ac695d69924ccd1bd3427be80d875dfe0c4d428d
factubsio <65080026+factubsio@users.noreply.github.com>
2022-03-05 16:40:04 +0000

    copy icons properly

--- BubbleTweaks/Crusade.cs
+++ BubbleTweaks/Crusade.cs
@@ -332,53 +332,57 @@ namespace BubbleTweaks {
 
         public static void TryInitArmyUITweaks() {
             if (GlobalMapUI.Instance != null && GlobalMapUI.Instance.transform.Find("ArmyHUDPCView/Background/BUBBLEBUT0") == null) {
-                Main.Log("Initializing Crusade UI tweaks");
-                var button = GlobalMapUI.Instance.transform.Find("ArmyHUDPCView/Background/InfoBlock");
+                try {
+                    Main.Log("Initializing Crusade UI tweaks");
+                    var button = GlobalMapUI.Instance.transform.Find("ArmyHUDPCView/Background/InfoBlock");
 
-                for (int i = 0; i < button.parent.childCount; i++) {
-                    if (button.parent.GetChild(i).name.StartsWith("BUBBLE")) {
-                        GameObject.Destroy(button.parent.GetChild(i).gameObject);
-                    }
-                }
-
-                var buttonNew = GameObject.Instantiate(button.gameObject, button.parent);
-                var innerButton = buttonNew.transform.Find("SettingsButton");
-
-                disbandButton = innerButton.GetComponent<OwlcatButton>();
-                var layer = disbandButton.m_CommonLayer[0];
-                var tex = innerButton.GetComponent<Image>();
-                var baseSprite = AssetLoader.LoadInternal("icons", "disband_army.png", new Vector2Int(128, 128));
-                var pressedSprite = AssetLoader.LoadInternal("icons", "disband_army_click.png", new Vector2Int(128, 128));
-                var highlightedSprite = AssetLoader.LoadInternal("icons", "disband_army_hover.png", new Vector2Int(128, 128));
-                tex.sprite = baseSprite;
-                layer.m_SpriteState.highlightedSprite = highlightedSprite;
-                layer.m_SpriteState.pressedSprite = pressedSprite;
-                layer.m_SpriteState.selectedSprite = baseSprite;
-
-                disbandButton.m_OnSingleLeftClick = new Button.ButtonClickedEvent();
-                disbandButton.m_OnSingleLeftClick.AddListener(() => {
-                    Main.Log("Trying to kill army?");
-                    if (armySelectionHandler.Army == null)
-                        return;
-                    if (armySelectionHandler.Army.Data.m_LeaderGuid != null && armySelectionHandler.Army.Data.m_LeaderGuid.Length > 0) {
-                        Main.Log("Trying to disband an army with a general");
-                        UIUtility.ShowMessageBox("You cannot disband an army that has a General", Kingmaker.UI.MessageModalBase.ModalType.Message, (buttonType) => {
-                        });
-                        return;
-                    }
-                    UIUtility.ShowMessageBox("Are you sure you want to disband this army?", Kingmaker.UI.MessageModalBase.ModalType.Dialog, (buttonType) => {
-                        if (buttonType == Kingmaker.UI.MessageModalBase.ButtonType.Yes) {
-                            Main.Log($"Disposing of army: {armySelectionHandler.Army.Data.ArmyName.ArmyName}");
-                            armySelectionHandler.Army.Data.RemoveAllSquads();
-                            Game.Instance.Player.GlobalMap.LastActivated.DestroyArmy(armySelectionHandler.Army);
+                    for (int i = 0; i < button.parent.childCount; i++) {
+                        if (button.parent.GetChild(i).name.StartsWith("BUBBLE")) {
+                            GameObject.Destroy(button.parent.GetChild(i).gameObject);
                         }
-                    }, null, 0, "Disband", "Cancel");
-                });
+                    }
 
-                var frame = buttonNew.transform as RectTransform;
-                frame.name = "BUBBLEBUT0";
-                frame.localPosition += new Vector3(0, frame.rect.height + 4, 0);
-                Main.Log("Finished installing Crusade UI Tweaks");
+                    var buttonNew = GameObject.Instantiate(button.gameObject, button.parent);
+                    var innerButton = buttonNew.transform.Find("SettingsButton");
+
+                    disbandButton = innerButton.GetComponent<OwlcatButton>();
+                    var layer = disbandButton.m_CommonLayer[0];
+                    var tex = innerButton.GetComponent<Image>();
+                    var baseSprite = AssetLoader.LoadInternal("icons", "disband_army.png", new Vector2Int(128, 128));
+                    var pressedSprite = AssetLoader.LoadInternal("icons", "disband_army_click.png", new Vector2Int(128, 128));
+                    var highlightedSprite = AssetLoader.LoadInternal("icons", "disband_army_hover.png", new Vector2Int(128, 128));
+                    tex.sprite = baseSprite;
+                    layer.m_SpriteState.highlightedSprite = highlightedSprite;
+                    layer.m_SpriteState.pressedSprite = pressedSprite;
+                    layer.m_SpriteState.selectedSprite = baseSprite;
+
+                    disbandButton.m_OnSingleLeftClick = new Button.ButtonClickedEvent();
+                    disbandButton.m_OnSingleLeftClick.AddListener(() => {
+                        Main.Log("Trying to kill army?");
+                        if (armySelectionHandler.Army == null)
+                            return;
+                        if (armySelectionHandler.Army.Data.m_LeaderGuid != null && armySelectionHandler.Army.Data.m_LeaderGuid.Length > 0) {
+                            Main.Log("Trying to disband an army with a general");
+                            UIUtility.ShowMessageBox("You cannot disband an army that has a General", Kingmaker.UI.MessageModalBase.ModalType.Message, (buttonType) => {
+                            });
+                            return;
+                        }
+                        UIUtility.ShowMessageBox("Are you sure you want to disband this army?", Kingmaker.UI.MessageModalBase.ModalType.Dialog, (buttonType) => {
+                            if (buttonType == Kingmaker.UI.MessageModalBase.ButtonType.Yes) {
+                                Main.Log($"Disposing of army: {armySelectionHandler.Army.Data.ArmyName.ArmyName}");
+                                armySelectionHandler.Army.Data.RemoveAllSquads();
+                                Game.Instance.Player.GlobalMap.LastActivated.DestroyArmy(armySelectionHandler.Army);
+                            }
+                        }, null, 0, "Disband", "Cancel");
+                    });
+
+                    var frame = buttonNew.transform as RectTransform;
+                    frame.name = "BUBBLEBUT0";
+                    frame.localPosition += new Vector3(0, frame.rect.height + 4, 0);
+                    Main.Log("Finished installing Crusade UI Tweaks");
+                } catch (Exception e) {
+                    Main.Error(e);
+                }
             }
         }
     }
