60942c7a6053676e5c642dfb9ce0c6759d95f6bc
TylerGoeringer <tyler.goeringer@gmail.com>
2023-01-03 16:47:49 -0800

    Add Interaction Highlight Toggle
    Logic is simple:
    - Prefix when highlight is turned on; if it's off let the method run, otherwise turn it off and skip the method
    - Prefix when highlight is turned off; if it's on skip the method, otherwise turn if on and let the method run (so that it treats it as the normal case)

---
+++ BubbleTweaks/InteractionHighlightToggle.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using Kingmaker.Controllers.MapObjects;
+using System;
+
+namespace BubbleTweaks {
+    internal class InteractionHighlightToggle {
+        [HarmonyPatch(typeof(InteractionHighlightController))]
+        static class InteractionHighlightController_Patch {
+            [HarmonyPatch(nameof(InteractionHighlightController.HighlightOn)), HarmonyPrefix]
+            static bool HighlightOn(InteractionHighlightController __instance) {
+                try {
+                    if (__instance.m_Inactive || !BubbleSettings.Instance.InteractionHighlight.GetValue())
+                        return true;
+
+                    if (__instance.m_IsHighlighting) {
+                        __instance.m_IsHighlighting = false;
+                        __instance.HighlightOff();
+                        return false;
+                    }
+
+                    return true;
+                }
+                catch (Exception e) {
+                    Main.Error(e, "Failed while processing interaction highlight toggle on.");
+                }
+                return true;
+            }
+
+            [HarmonyPatch(nameof(InteractionHighlightController.HighlightOff)), HarmonyPrefix]
+            static bool HighlightOff(InteractionHighlightController __instance) {
+                try {
+                    if (__instance.m_Inactive || !BubbleSettings.Instance.InteractionHighlight.GetValue())
+                        return true;
+
+                    if (__instance.m_IsHighlighting) {
+                        return false;
+                    }
+
+                    // Set this to true so the normal logic will process to disable higlights
+                    __instance.m_IsHighlighting = true;
+                    return true;
+                } catch (Exception e) {
+                    Main.Error(e, "Failed while processing interaction highlight toggle off.");
+                }
+                return true;
+            }
+        }
+    }
+}
